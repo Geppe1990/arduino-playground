8d6dafead3e3a96a98172212e9ea3a51a5a2878e
Giuseppe Vigneri <geppe1990@gmail.com>
2023-11-13 14:12:12 +0100

    Update r4-wifi.ino

--- r4-wifi/r4-wifi.ino
+++ r4-wifi/r4-wifi.ino
@@ -1,5 +1,7 @@
 #include "secrets.h"
 #include <WiFiS3.h>
+#include <SPI.h>
+#include <HttpClient.h>
 
 void wifiConnect() {
   WiFi.begin(MY_WIFI_SSID, MY_WIFI_PASS);
@@ -16,36 +18,85 @@ void setup() {
   char endpoint[] = "jsonplaceholder.typicode.com";
   char path[] = "/posts/1";
   int port = 80;
+  int err = 0;
+  const int networkTimeout = 30*1000;
+  const int networkDelay = 1000;
   Serial.begin(115200);
   wifiConnect();
   delay(1000);
 
   WiFiClient client;
-  if (!client.connect(endpoint, port)) {
-    Serial.println("[Endpoint "+ (String)endpoint +" non raggiunto]");
-  } else {
-    Serial.println("[Endpoint "+ (String)endpoint +" raggiunto]");
-    client.println("GET " + (String)path +" HTTP/1.1");
-    client.println("Host: " + (String)endpoint);
-    client.println("Connection: close");
-    client.println("");
+  HttpClient http(client);
+  err = http.get(endpoint, path);
 
-    unsigned long t1 = millis();
-    while (client.available() == 0) {
-      if ((millis() - t1) > 5000) {
-         Serial.println("timeout");
-         client.stop();
+  if (err == 0)
+  {
+    Serial.println("startedRequest ok");
+
+    err = http.responseStatusCode();
+    if (err >= 0)
+    {
+      Serial.print("Got status code: ");
+      Serial.println(err);
+
+      // Usually you'd check that the response code is 200 or a
+      // similar "success" code (200-299) before carrying on,
+      // but we'll print out whatever response we get
+
+      err = http.skipResponseHeaders();
+      if (err >= 0)
+      {
+        int bodyLen = http.contentLength();
+        Serial.print("Content length is: ");
+        Serial.println(bodyLen);
+        Serial.println();
+        Serial.println("Body returned follows:");
+      
+        // Now we've got to the body, so we can print it out
+        unsigned long timeoutStart = millis();
+        char c;
+        // Whilst we haven't timed out & haven't reached the end of the body
+        while ( (http.connected() || http.available()) && ((millis() - timeoutStart) < networkTimeout) )
+        {
+          if (http.available())
+          {
+              c = http.read();
+              // Print out this character
+              Serial.print(c);
+
+              bodyLen--;
+              // We read something, reset the timeout counter
+              timeoutStart = millis();
+          }
+          else
+          {
+              // We haven't got any data, so let's pause to allow some to
+              // arrive
+              delay(networkDelay);
+          }
+        }
+      }
+      else
+      {
+        Serial.print("Failed to skip response headers: ");
+        Serial.println(err);
       }
     }
-    
-    //ricezione dati 
-    Serial.println("Stampo la risposta:\n");
-    while (client.available()) {
-      String line = client.readStringUntil('\r');
-      Serial.print(line);
+    else
+    {    
+      Serial.print("Getting response failed: ");
+      Serial.println(err);
     }
-    client.stop();
   }
+  else
+  {
+    Serial.print("Connect failed: ");
+    Serial.println(err);
+  }
+  http.stop();
+
+  // And just stop, now that we've tried a download
+  while(1);
 }
 
 void loop() {
